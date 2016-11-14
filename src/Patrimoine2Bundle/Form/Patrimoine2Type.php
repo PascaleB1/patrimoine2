36e52bcbdc188cf0283b5be4a62a0d41502ef471
PascaleB1 <barboteaupascale@yahoo.fr>
2016-11-14 19:40:45 +0100

    création pages index et edit

--- src/Patrimoine2Bundle/Form/Patrimoine2Type.php
+++ src/Patrimoine2Bundle/Form/Patrimoine2Type.php
@@ -5,6 +5,7 @@ namespace Patrimoine2Bundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class Patrimoine2Type extends AbstractType
 {
@@ -13,7 +14,15 @@ class Patrimoine2Type extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('titre1')->add('article1')->add('titre2')->add('image1')->add('image2')        ;
+        $builder
+            ->add('titre1')
+            ->add('article1')
+            ->add('titre2')
+            ->add('article2')
+            ->add('image1')
+            ->add('image2')
+            ->add('file1', FileType::class, array('label' => 'insérer une image', 'required' => false))
+            ->add('file2', FileType::class, array('label' => 'insérer une image', 'required' => false));
     }
     
     /**
